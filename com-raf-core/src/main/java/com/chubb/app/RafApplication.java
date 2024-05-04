19d349a970815503929d3a92560ca573677995e7
Eswar <eswarpalacharla@gmail.com>
2024-05-04 14:29:47 +0530

    Add renovate bot

---
+++ com-raf-core/src/main/java/com/chubb/app/RafApplication.java
@@ -0,0 +1,11 @@
+package com.chubb.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RafApplication {
+
+    public static void main(String[] args) {
+        System.out.println("eswar");
+    }
+}
